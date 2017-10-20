405abf3fe0c24e5b7562b433385734c33c654aea
Bohdan <bogdan.belenok@gmail.com>
2017-10-20 16:58:20 +0300

    dirty hax for firefox

--- index.rb
+++ index.rb
@@ -1,5 +1,6 @@
 require_relative './utils/deps.rb'
 
-scr = Screenshooter.new('Chrome')
+browser = browser_factory('Firefox')
+scr = Screenshooter.new(browser)
 scr.generate_local_screenshots()
-
+browser.close()
--- src/screenshooter.rb
+++ src/screenshooter.rb
@@ -1,19 +1,22 @@
 class Screenshooter
-  def initialize(browser_name, height = 2000)
-    @height = height
-    @browser = browser_factory(browser_name)
+  def initialize(browser)
+    @browser = browser
+    #hack for firefox screenshoots
+    @vertical_offset = @browser.driver
+                               .is_a?(Selenium::WebDriver::Firefox::Marionette::Driver) ? 100 : 0
     @widths = CONFIG['widths']
+    @folder_tree = FolderTree.new
   end
 
   def generate_local_screenshots
-    folder_tree = FolderTree.new
-    folder_tree.generate_folder_tree
-    progressbar = ProgressBar.new(folder_tree.component_paths.length * @widths.length)
-    
-    @widths.each do |width| 
-    folder_tree.component_paths.each do |component_name, path|
-      @browser.goto('file://' + path + '/index.html')
-        @browser.window.resize_to(width, get_page_height(@browser))
+    progressbar = ProgressBar.new(@folder_tree.component_paths.length * @widths.length)
+    @folder_tree.generate_folder_tree
+
+    @widths.each do |width|
+      @browser.window.resize_to(width, 0)     #hack for firefox screenshoots
+      @folder_tree.component_paths.each do |component_name, path|
+        @browser.goto('file://' + path + '/index.html')
+        @browser.window.resize_to(width, get_page_height(@browser) + @vertical_offset)
         pict_path = './screenshots/' + component_name + '/' + width.to_s + '.png'
         @browser.driver.save_screenshot(pict_path)
         progressbar.increment!
