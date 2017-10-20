d8adede4fcb9cb8224f87574395737f8b66c5f7c
Bohdan <bogdan.belenok@gmail.com>
2017-10-20 09:46:38 +0300

    add firefox -headless

--- utils/utils.rb
+++ utils/utils.rb
@@ -10,7 +10,10 @@ def browser_factory(browser_name)
       --disable-gpu
     ]
   when 'Firefox'
-    Watir::Browser.new :firefox
+    options = Selenium::WebDriver::Firefox::Options.new(
+      args: ['-headless']
+    )
+    Watir::Browser.new :firefox, options: options
   when 'Safari'
     Watir::Browser.new :safari
   end
@@ -22,4 +25,8 @@ end
 
 def replace_all_texts(browser, selector, text)
   browser.execute_script("document.querySelectorAll('#{selector}').forEach(item => { item.innerHTML = '#{text}'})")
+end
+
+def get_page_height
+  browser.execute_script('return document.body.clientHeight')
 end
