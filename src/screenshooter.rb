18f6f2c0205161db74164d8c4795881c26e791d9
Bohdan <bogdan.belenok@gmail.com>
2017-10-23 14:14:56 +0300

    separate path

--- src/screenshooter.rb
+++ src/screenshooter.rb
@@ -1,26 +1,35 @@
 class Screenshooter
   def initialize(browser)
     @browser = browser
-    #hack for firefox screenshoots
-    @vertical_offset = @browser.driver
-                               .is_a?(Selenium::WebDriver::Firefox::Marionette::Driver) ? 100 : 0
+    # HACK: for firefox screenshoots
+    browser_configure(browser)
     @widths = CONFIG['widths']
     @folder_tree = FolderTree.new
   end
 
+  def browser_configure
+    if @browser.driver.is_a?(Selenium::WebDriver::Firefox::Marionette::Driver)
+      @vertical_offset = 100
+      @browser_name = 'Firefox'
+    elsif @browser.driver.is_a?(Selenium::WebDriver::Chrome::Driver)
+      @vertical_offset = 0
+      @browser_name = 'Chrome'
+    end
+  end
+
   def generate_local_screenshots
     progressbar = ProgressBar.new(@folder_tree.component_paths.length * @widths.length)
-    @folder_tree.generate_folder_tree
+    @folder_tree.generate_folder_tree(@browser_name)
 
     @widths.each do |width|
-      @browser.window.resize_to(width, 0)     #hack for firefox screenshoots
+      @browser.window.resize_to(width, 0) # HACK: for firefox screenshoots
       @folder_tree.component_paths.each do |component_name, path|
         @browser.goto('file://' + path + '/index.html')
         @browser.window.resize_to(width, get_page_height(@browser) + @vertical_offset)
-        pict_path = './screenshots/' + component_name + '/' + width.to_s + '.png'
-        @browser.driver.save_screenshot(pict_path)
+        @browser.driver.save_screenshot(@folder_tree.get_pict_path(component_name, width))
         progressbar.increment!
       end
     end
   end
+  private :browser_configure
 end
