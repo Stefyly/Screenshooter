c042516f3ed00cf0c072d73c6eeae68a5bec72fe
Bohdan <bogdan.belenok@gmail.com>
2017-11-28 11:09:46 +0200

    blocks from folder

--- src/folder_tree/state_folder_tree.rb
+++ src/folder_tree/state_folder_tree.rb
@@ -1,23 +1,61 @@
 class StateFolderTree < FolderTree
   # Set filename in format [name_[0-9] - if u need to run states only for 1 block
-  def initialize(file = nil)
+  def initialize(args = nil)
     FileUtils.mkpath('./screenshots/states') unless File.directory?('./screenshots/states')
-    @file = file
+    @args = args
     @current_folder = './screenshots/states'
-    @blocks = block_paths
+    #@blocks = block_paths
   end
 
   # generate list of folder and subfolder names form ./states in format folder/subfolder
   # input  -> header_1.yml
   # output -> d-1/header
+
   def existed_states
-    if @file.nil?
-      folders = Dir.glob('**/wireframes/**/*.yml')
-      folders.map { |a| a.gsub!('states/wireframes/', '').gsub!('.yml', '') }
-    else
-      [@file]
+    mode = if @args.mode.nil?
+             'wireframes'
+           else
+             @args.mode
+           end
+
+      if @args.folder.nil? && @args.file.nil?
+        folders = Dir.glob("**/#{mode}/**/*.yml")
+        p folders        
+        folders.map { |a| a.gsub!("states/#{mode}/", '').gsub!('.yml', '') }
+        p 'run full'
+        folders       
+      elsif !@args.folder.nil?
+        p 'run only one folder'        
+        folders = Dir.glob("**/#{mode}/#{@args[:folder]}/*.yml")
+        folders.map { |a| a.gsub!("states/#{mode}/", '').gsub!('.yml', '') }
+      else
+      p 'run single file'
+        [@args.file]
+      end
+  end
+
+  def blocks_from_folder
+      if !@args.folder.nil?
+        folder_name = @args.folder
+        mode = mode_from_name(folder_name)
+      elsif 
+        folder_name = '**'
+        mode = mode_from_name(@args.mode)
+      end
+      folders = Dir.glob("**/#{mode}/#{folder_name}/*.yml")
+      folders.map { |a| a.gsub!("states/#{mode}/", '').gsub!('.yml', '') }
+  end
+
+  def mode_from_name(name)
+    if name[0] == 'w'
+      'wireframes'
+    elsif name[0] == 'd'
+      'designs'
     end
   end
+    
+    
+  
 
   # get hash of blocks names and its full path which have configs in /states folder
   # in format
